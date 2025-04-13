f33428d8fdcacde5f643b552fbc72786d26f0cb8
dev <mindcraftchamp9@gmail.com>
2025-04-13 17:33:49 -0500

    analytics for vercel added

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,11 @@
 import "@/styles/globals.css";
+import { Analytics } from '@vercel/analytics/react'; // import the package
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics /> {/* Add it here */}
+    </>
+  );
 }
